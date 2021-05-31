fe7df10ab7cfe3b7f09d02df67ab73b491df8e93
Junruoyu Zheng <zhengjry@126.com>
2021-05-31 21:44:40 +0800

    add: game state machine

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sonar_captain
+{
+    class Program
+    {
+        enum GameState
+        {
+            START, INITIAL, WAIT4START, OPERATION, 
+            MAP, MASK, ENGINEER, WORKER, ACTION,
+            DETECT, CHECKOUT, WAITING, END
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
