07ca5b47561cc16e7ab48745efe6fa4f26adf4dc
Junruoyu Zheng <zhengjry@126.com>
2021-05-31 22:35:33 +0800

    mv: extract key check logic and move handler to game class

--- Game.cs
+++ Game.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace sonar_captain
 {
     class Game
     {
         public string room {get; private set;}
 
-        public bool SetRoomName(string roomName)
+        private bool SetRoomName(string roomName)
         {
             if (roomName.Length == 6)
             {
@@ -14,6 +16,19 @@ namespace sonar_captain
             else
             {
                 return false;
+            }
+        }
+        public void StartHandler()
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入6位房间号：");
+                string roomName = Console.ReadLine();
+                if (SetRoomName(roomName))
+                {
+                    break;
+                }
+                Console.Write("房间号不合法，");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace sonar_captain
             MAP, MASK, ENGINEER, WORKER, ACTION,
             DETECT, CHECKOUT, WAITING, END
         }
+        static ConsoleKey GetKey(params ConsoleKey[] keys)
+        {
+            while (true)
+            {
+                ConsoleKey command = Console.ReadKey().Key;
+                foreach (ConsoleKey key in keys)
+                {
+                    if (command == key)
+                    {
+                        return command;
+                    }
+                }
+                Console.WriteLine("命令不正确，请重新输入：");
+            }
+        }
         static void Main(string[] args)
         {
             Game game = new Game();
@@ -19,26 +34,8 @@ namespace sonar_captain
                 if (gameState == GameState.START)
                 {
                     Console.WriteLine("深海谍影开始游戏。请输入c创建房间，或者a加入房间：");
-                    ConsoleKey command;
-                    while (true)
-                    {
-                        command = Console.ReadKey().Key;
-                        if (command == ConsoleKey.C || command == ConsoleKey.A)
-                        {
-                            break;
-                        }
-                        Console.WriteLine("命令必须是c或者a，请重新输入：");
-                    }
-                    while (true)
-                    {
-                        Console.WriteLine("请输入6位房间号：");
-                        string roomName = Console.ReadLine();
-                        if (game.SetRoomName(roomName))
-                        {
-                            break;
-                        }
-                        Console.Write("房间号不合法，");
-                    }
+                    ConsoleKey command = GetKey(ConsoleKey.C, ConsoleKey.A);
+                    game.StartHandler();
                     if (command == ConsoleKey.C)
                     {
                         gameState = GameState.INITIAL;
